037d1c0a7ba16cd9f9c9ae0a81b89a8d8f304457
Carlos Frias <carlos.a.frias@gmail.com>
2024-03-12 17:02:14 -0300

    remove ts tools

--- init.lua
+++ init.lua
@@ -803,12 +803,6 @@ require('lazy').setup({
   },
 
   {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
-  },
-
-  {
     'github/copilot.vim',
   },
 
