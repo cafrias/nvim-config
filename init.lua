53b62cab7f0601035001c3271be2c3b621610de9
Carlos Frias <carlos.a.frias@gmail.com>
2024-03-21 10:41:12 -0300

    set tabstop to 4

--- init.lua
+++ init.lua
@@ -151,6 +151,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Default 8 is too much, try 4
+vim.opt.tabstop = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
